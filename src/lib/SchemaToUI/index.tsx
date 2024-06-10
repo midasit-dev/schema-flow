454f00e8c33ca7934411e43beddebecb104fa718
lhy0118 <lhy0118@midasit.com>
2024-06-10 11:08:12 +0900

    Panel에 border 추가

--- src/lib/SchemaToUI/index.tsx
+++ src/lib/SchemaToUI/index.tsx
@@ -6,18 +6,18 @@ import { Canvas, Canvases, Layer } from '../Common/types';
 
 export default function SchemaToUI(props: { canvases: Canvases }) {
 	const { canvases } = props;
-	console.log('Schema', canvases);
+
 	return (
 		<React.Fragment>
 			{/* canvases is object */}
 			{Object.keys(canvases).map((canvasId: string) => {
-				console.log('canvasId', canvasId);
 				const canvas: Canvas = canvases[canvasId];
 				return (
 					<Panel
 						key={'PanelCanvas-' + uuidv4().slice(0, 8)}
 						width={canvas.width}
 						height={canvas.height}
+						border={'1px solid #c1c1c3'}
 					>
 						{canvas.layers.map((layer: Layer, index: number) => {
 							return <ToComponent key={index} layer={layer} />;
