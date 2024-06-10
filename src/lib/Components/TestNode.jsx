349c3a862d9f8214069780555a6f8be454f5a46b
lhy0118 <lhy0118@midasit.com>
2024-06-10 11:07:04 +0900

    TestNode Recoil 참조하는 코드 추가

--- src/lib/Components/TestNode.jsx
+++ src/lib/Components/TestNode.jsx
@@ -3,199 +3,15 @@ import { Handle, useStore, Position, useReactFlow } from 'reactflow';
 import { Button, Typography } from '@midasit-dev/moaui';
 import SchemaToUI from '../SchemaToUI';
 
-const dimensionAttrs = ['width', 'height'];
+//recoil
+import { useRecoilValue } from 'recoil';
+import { SelectedSchema } from '../RecoilAtom/recoilState';
 
-const exampleSchema = {
-	canvas_1: {
-		width: 640,
-		height: 256,
-		layers: [
-			{
-				id: '1-FloatingBox',
-				type: 'FloatingBox',
-				props: {
-					x: 0,
-					y: 0,
-					width: 640,
-					height: 64,
-					guideBoxProps: {
-						width: 'inherit',
-						height: 'inherit',
-						id: '1-FloatingBox',
-						spacing: 1,
-						center: true,
-					},
-				},
-				children: [
-					{
-						id: '1-Typography',
-						type: 'Typography',
-						props: {
-							children: 'text_to_plate_mesh',
-							width: 'auto',
-							height: 'auto',
-							variant: 'h1',
-							color: 'primary',
-							size: 'large',
-						},
-						children: [],
-					},
-					{
-						id: '2-Typography',
-						type: 'Typography',
-						props: {
-							children:
-								'Convert the text to a plate mesh and upload the image as a triangular mesh to MIDAS CIVIL.',
-							width: 'auto',
-							height: 'auto',
-							variant: 'body1',
-							color: 'primary',
-							size: 'small',
-						},
-						children: [],
-					},
-				],
-				parent: null,
-			},
-			{
-				id: '2-FloatingBox',
-				type: 'FloatingBox',
-				props: {
-					x: 0,
-					y: 64,
-					width: 640,
-					height: 64,
-					guideBoxProps: {
-						width: 'inherit',
-						height: 'inherit',
-						id: '2-FloatingBox',
-						spacing: 1,
-						center: true,
-					},
-				},
-				children: [
-					{
-						id: '2-TextField',
-						type: 'TextField',
-						props: {
-							title: 'text',
-							placeholder: 'Placeholder',
-							wrappedWidth: '200px',
-							width: '150px',
-							height: '30px',
-						},
-						children: [],
-					},
-				],
-				parent: null,
-			},
-			{
-				id: '3-FloatingBox',
-				type: 'FloatingBox',
-				props: {
-					x: 0,
-					y: 128,
-					width: 640,
-					height: 64,
-					guideBoxProps: {
-						width: 'inherit',
-						height: 'inherit',
-						id: '3-FloatingBox',
-						spacing: 1,
-						center: true,
-					},
-				},
-				children: [
-					{
-						id: '3-TextField',
-						type: 'TextField',
-						props: {
-							title: 'insert',
-							placeholder: 'Placeholder',
-							wrappedWidth: '200px',
-							width: '150px',
-							height: '30px',
-						},
-						children: [],
-					},
-				],
-				parent: null,
-			},
-			{
-				id: '4-FloatingBox',
-				type: 'FloatingBox',
-				props: {
-					x: 0,
-					y: 192,
-					width: 640,
-					height: 64,
-					guideBoxProps: {
-						width: 'inherit',
-						height: 'inherit',
-						id: '4-FloatingBox',
-						spacing: 1,
-						center: true,
-					},
-				},
-				children: [
-					{
-						id: '4-TextField',
-						type: 'TextField',
-						props: {
-							title: 'height',
-							placeholder: 'Placeholder',
-							wrappedWidth: '200px',
-							width: '150px',
-							height: '30px',
-						},
-						children: [],
-					},
-				],
-				parent: null,
-			},
-		],
-	},
-	canvas_run: {
-		width: 250,
-		height: 100,
-		layers: [
-			{
-				id: '6-FloatingBox',
-				type: 'FloatingBox',
-				props: {
-					x: 0,
-					y: 0,
-					width: 220,
-					height: 64,
-					guideBoxProps: {
-						width: 'inherit',
-						height: 'inherit',
-						id: '6-FloatingBox',
-						spacing: 1,
-						center: true,
-					},
-				},
-				children: [
-					{
-						id: '6-Button',
-						type: 'Button',
-						props: {
-							children: '실행하기',
-							width: '120px',
-							variant: 'contained',
-							color: 'negative',
-						},
-						children: [],
-					},
-				],
-				parent: null,
-			},
-		],
-	},
-};
+const dimensionAttrs = ['width', 'height'];
 
 export default function TestNode(props) {
 	const { setNodes } = useReactFlow();
+	const selectedSchema = useRecoilValue(SelectedSchema);
 
 	// React.useEffect(() => {
 	// 	const UI = SchemaToUI(exampleSchema);
@@ -236,7 +52,7 @@ export default function TestNode(props) {
 	return (
 		<>
 			<div>
-				<SchemaToUI canvases={exampleSchema} />
+				<SchemaToUI canvases={selectedSchema} />
 			</div>
 			<Handle type='target' position={Position.Top} />
 		</>
