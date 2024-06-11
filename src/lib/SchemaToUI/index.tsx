c8e03ec85186b57843f5d91913659a84b7d171d1
lhy0118 <lhy0118@midasit.com>
2024-06-11 20:50:42 +0900

    Schema UI 컴포넌트 생성 시 Title 영역 추가되도록 수정.

--- src/lib/SchemaToUI/index.tsx
+++ src/lib/SchemaToUI/index.tsx
@@ -10,16 +10,43 @@ export default function SchemaToUI(props: { canvas: Canvas }) {
 	return (
 		<React.Fragment>
 			{/* canvases is object */}
-			<Panel
+			<div
+				style={{
+					width: '100%',
+					height: 26,
+					backgroundColor: '#000000',
+					borderTopLeftRadius: '5px',
+					borderTopRightRadius: '5px',
+					color: '#fff',
+					display: 'flex',
+					justifyContent: 'flex-start',
+					alignItems: 'center',
+					paddingLeft: '10px',
+					fontSize: '12px',
+					fontWeight: '400',
+					fontFamily: 'pretendard',
+				}}
+			>
+				Title
+			</div>
+			<div
 				key={'PanelCanvas-' + uuidv4().slice(0, 8)}
-				width={canvas.width}
-				height={canvas.height}
-				border={'1px solid #c1c1c3'}
+				style={{
+					width: canvas.width,
+					height: canvas.height,
+					borderLeft: '1px solid #c1c1c3',
+					borderRight: '1px solid #c1c1c3',
+					borderBottom: '1px solid #c1c1c3',
+					backgroundColor: '#fff',
+					position: 'relative',
+					borderBottomLeftRadius: '5px',
+					borderBottomRightRadius: '5px',
+				}}
 			>
 				{canvas.layers.map((layer: Layer, index: number) => {
 					return <ToComponent key={index} layer={layer} />;
 				})}
-			</Panel>
+			</div>
 		</React.Fragment>
 	);
 }
