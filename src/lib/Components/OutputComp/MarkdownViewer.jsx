0211c71a5844e25ae8478595861fb6389c25d887
bschoi <bschoi@midasit.com>
2024-08-09 14:40:44 +0900

    markdown 확대 축소를 위한 코드 추가

--- src/lib/Components/OutputComp/MarkdownViewer.jsx
+++ src/lib/Components/OutputComp/MarkdownViewer.jsx
@@ -1,3 +1,4 @@
+import React, { useState } from 'react';
 import remarkMath from 'remark-math';
 import rehypeKatex from 'rehype-katex';
 import remarkGfm from 'remark-gfm'; // 추가된 플러그인
@@ -7,8 +8,25 @@ import 'katex/dist/katex.min.css';
 import { GuideBox } from '@midasit-dev/moaui-components-v1';
 
 export default function MarkdownViewer(props) {
+	const [scale, setScale] = useState(1); // 확대/축소 비율을 상태로 관리
+
+	const handleWheel = (event) => {
+		if (event.ctrlKey) {
+			// Ctrl 키가 눌렸을 때만 확대/축소
+			event.preventDefault(); // 기본 휠 동작 방지 (예: 페이지 스크롤)
+			if (event.deltaY < 0) {
+				setScale((prevScale) => Math.min(prevScale + 0.1, 3)); // 최대 3배 확대
+			} else {
+				setScale((prevScale) => Math.max(prevScale - 0.1, 0.5)); // 최소 0.5배 축소
+			}
+		}
+	};
+
 	return (
-		<GuideBox>
+		<GuideBox
+			style={{ padding: '20px', transform: `scale(${scale})`, transformOrigin: '0 0' }}
+			onWheel={handleWheel} // 마우스 휠 이벤트 핸들러
+		>
 			<ReactMD
 				remarkPlugins={[remarkMath, remarkGfm]} // remark-gfm 추가
 				rehypePlugins={[rehypeKatex, rehypeRaw]}
@@ -21,15 +39,15 @@ export default function MarkdownViewer(props) {
 				}}
 				components={{
 					table: ({ children }) => (
-						<table style={{ width: '100%', borderCollapse: 'collapse' }}>{children}</table>
+						<table style={{ width: '60%', borderCollapse: 'collapse' }}>{children}</table>
 					),
 					th: ({ children }) => (
-						<th style={{ border: '1px solid #ddd', padding: '12px', backgroundColor: '#f2f2f2' }}>
+						<th style={{ border: '1px solid #ddd', padding: '8px', backgroundColor: '#f2f2f2' }}>
 							{children}
 						</th>
 					),
 					td: ({ children }) => (
-						<td style={{ border: '1px solid #ddd', padding: '12px', textAlign: 'left' }}>
+						<td style={{ border: '1px solid #ddd', padding: '8px', textAlign: 'left' }}>
 							{children}
 						</td>
 					),
