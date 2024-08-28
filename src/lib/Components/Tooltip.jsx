ad76c9839f0d0e1355aa84f3a0824f2c6e6aceba
lhy0118 <lhy0118@midasit.com>
2024-08-28 10:58:26 +0900

    Skeleton 컴포넌트 분리 및 Tooltip JSX/CSS 추가

---
+++ src/lib/Components/Tooltip.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+// css
+import './Tooltip.css';
+
+
+/**
+ * Tooltip component that displays a tooltip when hovering over the children element.
+ *
+ * @param {Object} props - The props for the Tooltip component.
+ * @param {React.ReactNode} props.children - The element that will trigger the tooltip (text or JSX).
+ * @param {React.ReactNode} props.content - The content of the tooltip (text or JSX).
+ * @param {('top'|'bottom'|'left'|'right')} props.position - The position of the tooltip relative to the children element.
+ * @default 'bottom'
+ * @returns {JSX.Element} The rendered tooltip component.
+ */
+function Tooltip ({ children, content, position = 'bottom' }) {
+
+	return (
+		<div className='tooltip-container'>
+			{children}
+			<div className={`tooltip tooltip-${position}`}>{content}</div>
+		</div>
+	);
+};
+
+export { Tooltip };
--- src/lib/Components/UserFlow.jsx
+++ src/lib/Components/UserFlow.jsx
@@ -1,31 +1,8 @@
 import React from 'react';
 
+import { UserFlowTemplateSkeleton } from './Skeleton/HomeSkeleton';
+
 import './UserFlow.css';
-
-function SkeletonElement({ type }) {
-	return <div className={`skeleton ${type}`}></div>;
-}
-
-function Skeleton() {
-	return (
-		<div className='skeleton-wrapper'>
-			<div className='skeleton-flow'>
-				<div className='skeleton-left-boxes'>
-					<div className='skeleton-box' />
-					<div className='skeleton-box' />
-				</div>
-				<div className='skeleton-line' />
-				<div className='skeleton-box' />
-				<div className='skeleton-line' />
-				<div className='skeleton-right-box'>
-					<div className='skeleton-box' />
-				</div>
-			</div>
-			<SkeletonElement type='title' />
-			<SkeletonElement type='text' />
-		</div>
-	);
-}
 
 export default function UserFlow() {
 	const [isLoading, setIsLoading] = React.useState(true);
@@ -38,7 +15,7 @@ export default function UserFlow() {
 
 	return (
 		<div style={{ display: 'grid', gridTemplateColumns: '1fr 1fr 1fr' }}>
-			{isLoading ? <Skeleton /> : <div>Flow</div>}
+			{isLoading ? <UserFlowTemplateSkeleton /> : <div>Flow</div>}
 		</div>
 	);
 }
