3c8b3bc3ae6506bbc9c49f310200a607820a33ee
lhy0118 <lhy0118@midasit.com>
2024-08-28 12:10:51 +0900

    login svg icon 추가 및 동작 추가 (login 페이지로 이동)

--- src/lib/Components/UserInfo.jsx
+++ src/lib/Components/UserInfo.jsx
@@ -1,19 +1,20 @@
 // UserInfo.jsx
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 // components
-import { SvgDropdown, SvgLogOut } from '../SVGComps';
+import { SvgDropdown, SvgLogOut, SvgLogin } from '../SVGComps';
 import { Tooltip } from './Tooltip';
 
 // css
 import './UserInfo.css';
 
-function DropDownButton() {
+function DropDownButton({ name = 'ANONYMOUS' }) {
 	return (
 		<div id='userDropDownButton'>
 			<div className='buttonContent'>
-				<div id='userIcon'>L</div>
-				<div id='userName'>LEEHAYOUNG</div>
+				<div id='userIcon'>{name.charAt(0).toUpperCase()}</div>
+				<div id='userName'>{name}</div>
 				<div className='userDropdownIconContainer'>
 					<SvgDropdown />
 				</div>
@@ -24,19 +25,37 @@ function DropDownButton() {
 
 function LogOutButton() {
 	return (
-		<Tooltip content="Logout" postion="down">
-			<div id='logOutButtonIcon'>
+		<Tooltip content='Logout' postion='down'>
+			<div className='logIn-OutButtonIcon' onClick={() => {}}>
 				<SvgLogOut />
 			</div>
 		</Tooltip>
 	);
 }
 
-export default function UserInfo() {
+function LogInButton() {
+	const navigate = useNavigate();
+
+	return (
+		<Tooltip content='Login' postion='down'>
+			<div
+				className='logIn-OutButtonIcon'
+				onClick={() => {
+					navigate('../login');
+				}}
+			>
+				<SvgLogin />
+			</div>
+		</Tooltip>
+	);
+}
+
+export default function UserInfo(props) {
+	const { name } = props;
 	return (
 		<div id='userInfo'>
-			<DropDownButton />
-			<LogOutButton />
+			<DropDownButton name={name} />
+			{name === undefined || name === null ? <LogInButton /> : <LogOutButton />}
 		</div>
 	);
 }
